afae31f8159e29304d893a2f268ff155d7e0e9fb
Tabina Shabir <tabinashabr@gmail.com>
2022-05-30 16:26:40 +0530

    Project Base Setup

---
+++ TheMovieVerse/TheMovieVerse.Model/Hall.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheMovieVerse.Model
+{
+    public class Hall
+    {
+    }
+}
---
+++ TheMovieVerse/TheMovieVerse.Model/Theatre.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TheMovieVerse.Model
+{
+    public class Theatre
+    {
+    }
+}
